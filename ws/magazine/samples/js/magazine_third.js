89c007a860479f372f7a83f8c5e76b4afa22ae05
mangaka585 <mangaka585@gmail.com>
2020-03-16 00:51:47 +0300

    Новый выпуск плюс обновления дизайна и поправки для SEO

--- ws/magazine/samples/js/magazine_third.js
+++ ws/magazine/samples/js/magazine_third.js
@@ -298,44 +298,6 @@ function moveBar(yes) {
 	}
 }
 
-function setPreview(view) {
-
-	var previewWidth = 112,
-		previewHeight = 73,
-		previewSrc = 'pages/preview.jpg',
-		preview = $(_thumbPreview.children(':first')),
-		numPages = (view==1 || view==$('#slider').slider('option', 'max')) ? 1 : 2,
-		width = (numPages==1) ? previewWidth/2 : previewWidth;
-
-	_thumbPreview.
-		addClass('no-transition').
-		css({width: width + 15,
-			height: previewHeight + 15,
-			top: -previewHeight - 30,
-			left: ($($('#slider').children(':first')).width() - width - 15)/2
-		});
-
-	preview.css({
-		width: width,
-		height: previewHeight
-	});
-
-	if (preview.css('background-image')==='' ||
-		preview.css('background-image')=='none') {
-
-		preview.css({backgroundImage: 'url(' + previewSrc + ')'});
-
-		setTimeout(function(){
-			_thumbPreview.removeClass('no-transition');
-		}, 0);
-
-	}
-
-	preview.css({backgroundPosition:
-		'0px -'+((view-1)*previewHeight)+'px'
-	});
-}
-
 // Width of the flipbook when zoomed in
 
 function largeMagazineWidth() {
