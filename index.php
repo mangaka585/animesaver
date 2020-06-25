271129522326d08c780530856115e77a954c50f9
mangaka585 <mangaka585@gmail.com>
2020-06-25 17:59:27 +0300

    Написал алгоритм выбора случайного аниме V2

--- index.php
+++ index.php
@@ -5,6 +5,16 @@ $random_num = $rest = substr($url_test, 0, 10);
 if($url_test == "/" || $random_num == "/index.php")
 {   //Отрисовываем index.php
     include "includes/new_design.php";
-}  else { //Отрисовываем anime.php
+}  else if($url_test == "/random")
+{   //Отрисаовываем рандомное аниме
+    $animeTableArray = mysqli_query($connection, "SELECT COUNT(*) FROM `anime`" );
+    $animeTableArrayCount = mysqli_fetch_array($animeTableArray);
+    $animeMaxId = (int) $animeTableArrayCount[0];
+    $animeRandId = rand(1, $animeMaxId);
+    $animeLinkArray = mysqli_query($connection, "SELECT * FROM `anime` WHERE `id` = $animeRandId ");
+    $animeObject = mysqli_fetch_assoc($animeLinkArray);
+    $animeLink = $animeObject['link'];
+    header( "Location: https://animesaver.ru/$animeLink" );
+}   else { //Отрисовываем anime.php
     include "includes/animepage_new_design.php";}
 mysqli_close($connection);?>
